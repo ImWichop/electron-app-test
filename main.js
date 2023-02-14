1c1ce0baa47ed8c2baa8da8a451e7888a3431a88
Luis Carrillo <luis.carrilloo.perez@gmail.com>
2023-02-14 13:04:50 -0600

    [ADD] updates working

--- main.js
+++ main.js
@@ -1,35 +1,56 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+const { app, BrowserWindow } = require("electron");
+const MainScreen = require("./mainScreen");
+const { autoUpdater } = require("electron-updater");
 
-function createWindow () {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    titleBarStyle: 'hidden',
-    titleBarOverlay: {
-      color: '#2f3241',
-      symbolColor: 'white'
-    },
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
-  win.maximize()
-  win.loadURL(path.join('file:',__dirname,'dist/electron-app/index.html'))
+let curWindow;
+
+//Basic flags
+autoUpdater.autoDownload = false;
+autoUpdater.autoInstallOnAppQuit = true;
+
+function createWindow() {
+  curWindow = new MainScreen();
 }
 
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
-})
+  app.on("activate", function () {
+    if (BrowserWindow.getAllWindows().length == 0) createWindow();
+  });
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+  autoUpdater.checkForUpdates();
+  curWindow.showMessage(`Checking for updates. Current version ${app.getVersion()}`);
+});
+
+/*New Update Available*/
+autoUpdater.on("update-available", (info) => {
+  curWindow.showMessage(`Update available. Current version ${app.getVersion()}`);
+  let pth = autoUpdater.downloadUpdate();
+  curWindow.showMessage(pth);
+});
+
+autoUpdater.on("update-not-available", (info) => {
+  curWindow.showMessage(`No update available. Current version ${app.getVersion()}`);
+});
+
+/*Download Completion Message*/
+autoUpdater.on("update-downloaded", (info) => {
+  curWindow.showMessage(`Update downloaded. Current version ${app.getVersion()}`);
+});
+
+autoUpdater.on("error", (info) => {
+  curWindow.showMessage(info);
+});
+
+
+
+
+//Global exception handler
+process.on("uncaughtException", function (err) {
+  console.log(err);
+});
+
+app.on("window-all-closed", function () {
+  if (process.platform != "darwin") app.quit();
+});
---
+++ mainScreen.js
@@ -0,0 +1,62 @@
+const { BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
+
+class MainScreen {
+  window;
+
+  position = {
+    width: 1000,
+    height: 600,
+    maximized: false,
+  };
+
+  constructor() {
+    this.window = new BrowserWindow({
+      width: this.position.width,
+      height: this.position.height,
+      removeMenu: true,
+      acceptFirstMouse: true,
+      autoHideMenuBar: true,
+      webPreferences: {
+        contextIsolation: true,
+        preload: path.join(__dirname, "./mainPreload.js"),
+      },
+    });
+
+    this.window.once("ready-to-show", () => {
+      this.window.show();
+
+      if (this.position.maximized) {
+        this.window.maximize();
+      }
+    });
+
+    this.handleMessages();
+
+    let wc = this.window.webContents;
+    wc.openDevTools({ mode: "undocked" });
+
+    this.window.loadURL(path.join('file:',__dirname,'dist/electron-app/index.html'))
+  }
+
+  showMessage(message) {
+    console.log("showMessage trapped");
+    console.log(message);
+    this.window.webContents.send("updateMessage", message);
+  }
+
+  close() {
+    this.window.close();
+    ipcMain.removeAllListeners();
+  }
+
+  hide() {
+    this.window.hide();
+  }
+
+  handleMessages() {
+    //Ipc functions go here.
+  }
+}
+
+module.exports = MainScreen;
